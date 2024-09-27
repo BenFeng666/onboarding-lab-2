93cbda71915ef55e2bec84e4bbd9221635aa9985
BenFeng666 <zhibinfeng2017@gmail.com>
2024-09-26 21:58:16 -0400

    finish exercise1

--- rtl/Exercise1.sv
+++ rtl/Exercise1.sv
@@ -12,5 +12,14 @@ module Exercise1 (
     input  [7:0] b,
     output [7:0] out
 );
+always @(*)begin    
+  case(op)   
+  2'b00: out=a+b;   
+  2'b01: out=a-b;   
+  2'b10: out=a&b;  
+  2'b11: out=a|b;   
+  default:out=8'b0;   
+  endcase 
+end
 
 endmodule
